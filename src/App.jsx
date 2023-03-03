9f48dc9952f282ba6d21459a30dbef1d723758b4
canduman00 <119858014+canduman00@users.noreply.github.com>
2023-03-03 18:26:51 +0300

    fixed cookie check

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const TaskContext = createContext();
 
 function App() {
   let placeholder = cookies.get("tasks-cookies");
-  placeholder.lenght < 0 ? placeholder = [] : null;
+  placeholder === undefined ? placeholder = [] : null;
 
   const [tasks, setTasks] = useState(placeholder);
 
